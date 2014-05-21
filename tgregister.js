42ef25098dd8cbef93efb09a2bf2ef62955b9f12
JaylynDawson <jaylyn.dawson@gmail.com>
2014-05-21 14:52:01 -0700

    Added cancel function

--- tgregister.js
+++ tgregister.js
@@ -6,6 +6,12 @@ $(function() { // call for jQuery
 var userButtonClicked = function() {
 
 }
+
+var reset = function() {
+	$('input').text().remove();
+	$('signin').show();
+	$('userDialog').hide();
+}
 	//validate email format/blank
 	//validate password length/not blank
 	//change sign in link to user's first name
@@ -13,13 +19,17 @@ var userButtonClicked = function() {
 //event handlers
 
 //signin/up button clicked (signInClicked)
-$(".userButton").on('click',userButtonClicked);
+$('.userButton').on('click',userButtonClicked);
 
 //signin tab clicked ()function
 //signup tab clicked function
 //submit clicked function
 //forgot pw clicked function
 //myAccount clicked function XXX not needed
+
 //cancel clicked function
-//password hover function
+$('cancel').on('click', reset);
+
+//password hover function //CSS
+
 });
