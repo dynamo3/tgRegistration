a08e225c68aa5c4cf7ceabcce2a75fd79e828031
Rick Larsen <ricklarsen4@yahoo.com>
2014-05-21 14:28:31 -0700

    added comments for event handlers. git push

--- tgregister.js
+++ tgregister.js
@@ -1,4 +1,25 @@
-$(function() {
+$(function() { // call for jQuery
+// variables
 
-	
+// function definitions
+// signInClick function
+var userButtonClicked = function() {
+
+}
+	//validate email format/blank
+	//validate password length/not blank
+	//change sign in link to user's first name
+
+//event handlers
+
+//signin/up button clicked (signInClicked)
+$(".userButton").on('click',userButtonClicked);
+
+//signin tab clicked ()function
+//signup tab clicked function
+//submit clicked function
+//forgot pw clicked function
+//myAccount clicked function XXX not needed
+//cancel clicked function
+//password hover function
 });
