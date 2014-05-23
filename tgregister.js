cd8380fc74f27ccf5fabda57a97145b91b220255
Rick Larsen <ricklarsen4@yahoo.com>
2014-05-23 12:28:08 -0700

    email validation works.

--- tgregister.js
+++ tgregister.js
@@ -6,12 +6,14 @@ var userButtonClicked = function() {
 	$('.userPopUp').show();
 	$('.userSignIn').show();
 	$('.userSignUp').hide();
+	$('.error').text("");
 	$('.email').focus();
 }	
 
 var reset = function(event) {
 	event.preventDefault();
-	$('input').val(""); 
+	$('input').val('');
+	$('.error').text(''); 
 	$('.userSignIn').show();
 	$('.userPopUp').hide();
 }
@@ -22,30 +24,33 @@ var reset = function(event) {
 // 	$('.signUp').hide();
 // }
 
-	// sign
-	var signInClicked = function() {
-		console.log('in');
-		$('.email').val("");
-		$('.password').val("");
-		$('.userSignIn').show();
-		$('.userSignUp').hide();
-		$('.signInTab').addClass('.activeTab');
-		$('.signUpTab').removeClass('.activeTab');
-		$('.email').focus();
-	}
+// sign
+var signInClicked = function() {
+	$('.email').val("");
+	$('.password').val("");
+	$('.userSignIn').show();
+	$('.userSignUp').hide();
+	$('.signInTab').addClass('.activeTab');
+	$('.signUpTab').removeClass('.activeTab');
+	$('.email').focus();
+}
 
-	var signUpClicked = function() {
-		console.log('up');
-		$('.firstName').text("");
-		$('.lastName').text("");
-		$('.email').text("");
-		$('.password').text("");
-		$('.userSignUp').show();
-		$('.userSignIn').hide();
-		$('.signUpTab').addClass('.activeTab');
-		$('.signInTab').removeClass('.activeTab');
-		$('.firstName').focus();
-	}
+var signUpClicked = function() {
+	$('.firstName').text("");
+	$('.lastName').text("");
+	$('.email').text("");
+	$('.password').text("");
+	$('.userSignUp').show();
+	$('.userSignIn').hide();
+	$('.signUpTab').addClass('.activeTab');
+	$('.signInTab').removeClass('.activeTab');
+	$('.firstName').focus();
+}
+
+//error function inserts error message and makes a red box
+var errorMessage = function(inputType, err) {
+	$('.error').text(inputType + " is " + err + ".");
+}
 //event handlers
 
 //signin/up button clicked (signInClicked)
@@ -61,68 +66,19 @@ $('.signUpTab').on('click',signUpClicked);
 // $('a.password').on('click', forgotPassword
 
 //password hover function 
-
-//submit clicked function
+//validate email field onblur
+$('.email').on('blur', function(){	
+	var email = $('.email').val();
+	var re = /^(.)+(\@)+(.)+(\.)+(.{1,64})$/;
+	if (!re.test(email)) {
+		errorMessage("email", "invalid");
+	} else {
+		$('.error').text("");
+	}
+});
 
 // //cancel clicked function
 $('.cancelButton').on('click', reset);
-
-
-
-
-// //Jaylyn's Workspace //
-// $('button.submit').on('click', function() {
-// 	for (i in $('.input')) {
-
-// 	}
-// 	if($('input.email').text() == '') {
-// 		error();
-// 	}
-// 	else {
-// 	reset();
-// 	$('header').html('')
-// 	}
-// });
-
-// //error function inserts error message and makes a red box
-// var errorMessage = function() {
-// 	console.log("this is the error");
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
