4df51330ce4ecd7aaf176124e1784288052d8ecf
denette <info@mybodyrenew.com>
2014-05-23 06:23:44 -0700

    Made a lot of changes to CSS and also some to html in the .php file, including issues 3 & 4

--- index.php
+++ index.php
@@ -47,17 +47,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	
-
 	<link rel="stylesheet" href="main.css">
 </head>
+
 <body>
 	<div class="mainContainer">
 	<!-- Start of Header section with User Nav -->
 		<header>
+			<div class="contactNav">(480) 643-7657</div>
 			<div class="userNav">
 				<div class="userButton">
-					<h4>User<button>Sign In / Sign Up</button></h4>
+					<div>
+						<span>user</span><button>Sign In / Sign Up</button></div>
 				</div>	
 
 				<div class="welcome">
@@ -70,7 +71,15 @@
 			</div>
 
 			<div class="subContainer">
-				Logo goes here
+				<a href="/" title="Return to the homepage" id="logo">
+	  				<img src="atg-logo.png" alt="AVNET Tech Games Logo" />
+				</a>
+				<nav class="subContainerNav">
+					<a class="currentGames" href="#">Current games</a>
+					<a class="sponsors" href="#">Sponsors</a>
+					<a class="faculty" href="#">Faculty</a>
+					<a class="students" href="#">Students</a>
+				</nav>
 			</div>	
 
 		
@@ -108,13 +117,28 @@
 		</header>
 	
 	<div class="contentContainer">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, incidunt illum earum. Dignissimos voluptatum maiores dicta aliquid. Molestiae, ratione, quas velit illum tempora recusandae quam nesciunt itaque beatae incidunt eum aliquid temporibus ipsam dolore veniam asperiores est impedit quibusdam dicta maiores consequatur magni nostrum delectus facilis similique aperiam omnis facere cum doloribus minus architecto eius necessitatibus assumenda. Asperiores, veritatis, magni obcaecati qui deserunt eos et totam mollitia fugit est nam quae ut eum eius sint dolores eligendi officia incidunt debitis expedita alias nemo culpa cumque minus iusto nobis nulla corrupti tempore quisquam quidem. Quaerat, doloremque excepturi non tempora consequatur atque perferendis natus autem eligendi accusamus saepe voluptatum illo necessitatibus sint quis ex exercitationem quidem quod voluptate quo! Sequi, eius, perferendis, odio placeat velit modi molestiae tenetur sapiente veritatis animi doloremque quia cupiditate voluptatibus delectus provident voluptate quaerat distinctio labore at inventore cum ipsum hic accusantium sunt corporis illum voluptatem dolorum amet magnam veniam ex perspiciatis deleniti saepe. Itaque, aperiam, quam, perferendis, temporibus soluta dolorum reiciendis autem ipsam alias et ipsum facere earum repellat ipsa ea sint rerum placeat asperiores id ratione animi officia quasi vitae totam veniam minima qui molestiae sunt est inventore voluptas facilis minus culpa. Consequatur, consectetur, eum aliquam excepturi ad quibusdam magni praesentium nesciunt earum voluptas nihil repellat saepe numquam voluptatum ex nam repellendus ullam assumenda quos consequuntur fuga expedita ea ducimus. Voluptatem, cumque, maxime, accusamus ab ullam maiores quae magnam impedit iste odit numquam reiciendis natus amet mollitia excepturi repellat expedita quos hic non quo neque possimus illum odio rerum repudiandae. Voluptates, nulla, dolor, veritatis ratione quasi labore cupiditate cumque deleniti porro magni eos ipsa vero iste consectetur facilis itaque necessitatibus dolorem blanditiis minus voluptatum quam velit assumenda quo quibusdam recusandae aliquam quas eligendi esse libero similique molestias soluta quos harum deserunt saepe odit hic impedit voluptatem sed praesentium. Facere, ratione, maxime commodi inventore dignissimos distinctio recusandae voluptate cumque vitae ullam praesentium nihil animi accusantium aperiam facilis repellat accusamus at nisi repellendus consequatur beatae tempora omnis. Corporis, totam dolor repudiandae. Cum, iste, amet. Vero, temporibus, obcaecati, earum corporis quos nemo necessitatibus rerum soluta a hic dolorem repellat quam voluptates animi unde et quibusdam fugiat excepturi quaerat assumenda explicabo nisi iure. Fugiat, tempore reprehenderit facilis dolores suscipit unde corporis nesciunt nulla modi iste enim earum alias quas rem pariatur error ad beatae ipsum sunt voluptas aut deserunt sequi Dignissimosissimos tempora veniam id maxime cum. Maiores, fuga, laudantium.</p>
+		<aside>
+			<span></span>
+			<ul class="asideLinks">
+				<li><a href="#">Schools</a></li>
+				<li><a href="#">Press</a></li>
+				<li><a href="#">Video</a></li>
+			</ul>
+		</aside>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, incidunt illum earum. Dignissimos voluptatum maiores dicta aliquid. Molestiae, ratione, quas velit illum tempora recusandae quam nesciunt itaque beatae incidunt eum aliquid temporibus ipsam dolore veniam asperiores est impedit quibusdam dicta maiores consequatur magni nostrum delectus facilis similique aperiam omnis facere cum doloribus minus architecto eius necessitatibus assumenda. Asperiores, veritatis, magni obcaecati qui deserunt eos et totam mollitia fugit est nam quae ut eum eius sint dolores eligendi officia incidunt debitis expedita alias nemo culpa cumque minus iusto nobis nulla corrupti tempore quisquam quidem. Quaerat, doloremque excepturi non tempora consequatur atque perferendis natus autem eligendi accusamus saepe voluptatum illo necessitatibus sint quis ex exercitationem quidem quod voluptate quo! Sequi, eius, perferendis, odio placeat velit modi molestiae tenetur sapiente veritatis animi doloremque quia cupiditate voluptatibus delectus provident voluptate quaerat distinctio labore at inventore cum ipsum hic accusantium sunt corporis illum voluptatem dolorum amet magnam veniam ex perspiciatis deleniti saepe. Itaque, aperiam, quam, perferendis, temporibus soluta dolorum reiciendis autem ipsam alias et ipsum facere earum repellat ipsa ea sint rerum placeat asperiores id ratione animi officia quasi vitae totam veniam minima qui molestiae sunt est inventore voluptas facilis minus culpa. Consequatur, consectetur, eum aliquam excepturi ad quibusdam magni praesentium nesciunt earum voluptas nihil repellat saepe numquam voluptatum ex nam repellendus ullam assumenda quos consequuntur fuga expedita ea ducimus.</p>
+		<p>Voluptatem, cumque, maxime, accusamus ab ullam maiores quae magnam impedit iste odit numquam reiciendis natus amet mollitia excepturi repellat expedita quos hic non quo neque possimus illum odio rerum repudiandae. Voluptates, nulla, dolor, veritatis ratione quasi labore cupiditate cumque deleniti porro magni eos ipsa vero iste consectetur facilis itaque necessitatibus dolorem blanditiis minus voluptatum quam velit assumenda quo quibusdam recusandae aliquam quas eligendi esse libero similique molestias soluta quos harum deserunt saepe odit hic impedit voluptatem sed praesentium. Facere, ratione, maxime commodi inventore dignissimos distinctio recusandae voluptate cumque vitae ullam praesentium nihil animi accusantium aperiam facilis repellat accusamus at nisi repellendus consequatur beatae tempora omnis. Corporis, totam dolor repudiandae. Cum, iste, amet. Vero, temporibus, obcaecati, earum corporis quos nemo necessitatibus rerum soluta a hic dolorem repellat quam voluptates animi unde et quibusdam fugiat excepturi quaerat assumenda explicabo nisi iure. Fugiat, tempore reprehenderit facilis dolores suscipit unde corporis nesciunt nulla modi iste enim earum alias quas rem pariatur error ad beatae ipsum sunt voluptas aut deserunt sequi Dignissimosissimos tempora veniam id maxime cum. Maiores, fuga, laudantium.</p>
 	</div>
 </div>
+
+<!-- Footer Section -->
+<footer>
+	<div>Contact Us<br>480-643-7657</div>
+</footer>
+
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="tgregister.js"></script>
-</body>
 
+</body>
 </html>
 
 
